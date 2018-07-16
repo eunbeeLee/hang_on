94c8d11bd697c73fb17fbd5710469fc581d52c16
이은비 <lob1224@hanmail.net>
2018-07-16 16:40:54 +0900

    75. 관리자 - 방관리 페이지(방검색 구현 완료)

--- hangOn/src/main/java/kr/co/hangOn/repository/mapper/RoomMapper.java
+++ hangOn/src/main/java/kr/co/hangOn/repository/mapper/RoomMapper.java
@@ -11,13 +11,13 @@ public interface RoomMapper {
 	int roomByJoinCode(String roomNo);
 	
 	//관리자 페이지- 생성된 방 목록 선택
-	List<Room> selectRoomByUser(int userNo);
+	List<Room> selectRoom(Room room);
 	
 	//방의 멤버 선택
 	List<RoomMember> selectMemberByRoom(RoomMember roomMember);
 	 
     //유저에 따른 방 갯수 조회
      int countRoomList(int userNo);
-    
+     
 }
 	
